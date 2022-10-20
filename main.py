8b18ecfb9b9fab01a4bb3f77bbc2aee98eea9534
John Teye Doku <johnteye2002@gmail.com>
2022-10-20 12:10:42 +0000

    Implement Ascii Art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# from playsound import playsound
-# playsound('C:\\Users\\USER\\Desktop\\diagnosis\\introaudio.mp3')
-
+from __future__ import print_function
+from playsound import playsound
+from ast import Break
 
 allergies_symptoms = ['eye irritation', 'runny nose', 'stuffy nose', 'puffy eyes', 'watery eyes', 'sneezing', 'inflamed throat', 'itchy nose']
 
@@ -10,7 +10,7 @@ allergies_treatment = ['Antihistamines', 'Decongestants', 'Anti-inflammatory age
 
 cold_flu_symptoms = ['fever', 'headache', 'fatigue', 'dry cough', 'runny nose', 'stuffy nose', 'sore throat', 'cough', ]
 
-cold_flu_causes = ['inflammation fo the membranes in the nose and throat']
+cold_flu_causes = ['inflammation of the membranes in the nose and throat']
 
 cold_flu_treatment = ['drink lots of fluids', 'eat a well-balanced diet', 'seek prescriptions for antiboitics']
 
@@ -27,9 +27,90 @@ malaria_causes = ['mosquito bites', 'organ transplant', 'a transfusion']
 malaria_treatment = ['antimalaria drugs']
 
 
+print(''' 
+
+ ______  _________ _______  _______  _        _______  _______ _________ _______ 
+(  __  \ \__   __/(  ___  )(  ____ \( (    /|(  ___  )(  ____ \\__   __/(  ____ \
+| (  \  )   ) (   | (   ) || (    \/|  \  ( || (   ) || (    \/   ) (   | (    \/
+| |   ) |   | |   | (___) || |      |   \ | || |   | || (_____    | |   | (_____ 
+| |   | |   | |   |  ___  || | ____ | (\ \) || |   | |(_____  )   | |   (_____  )
+| |   ) |   | |   | (   ) || | \_  )| | \   || |   | |      ) |   | |         ) |
+| (__/  )___) (___| )   ( || (___) || )  \  || (___) |/\____) |___) (___/\____) |
+(______/ \_______/|/     \|(_______)|/    )_)(_______)\_______)\_______/\_______)
+                                                                                 ''')
+playsound('C:\\Users\\USER\\Desktop\\diagnosis\\introaudio.mp3')
+
 user_symptoms = input("Enter your symptoms? \n").split(",")
-print(user_symptoms)
+
+
+# def knowCauses(disease):
+#     know_causes = input(f"Would you like to know the causes of {disease} \n Y/N: ")
+#     if know_causes == "Y":
+#         print(malaria_causes)
+#     else:
+#         Break()
+
+
+
+
+
 if set(user_symptoms).issubset(set(allergies_symptoms)):
     print("Likely to have allergic reactions")
-else:
+    know_causes = input("Would you like to know the causes of allergic reactions \n Y/N: ")
+    if know_causes == "Y":
+        print(*allergies_causes,sep='\n')
+    else:
+        Break()
+    know_treatment = input("Would you like to know the treatment of allergic reactions \n Y/N: ")
+    if know_treatment == "Y":
+        print(*allergies_treatment,sep='\n')
+    else:
+        Break()
+    
+elif set(user_symptoms).issubset(set(cold_flu_symptoms)):
     print("Likely to have cold or flu")
+    know_causes = input("Would you like to know the causes of cold and flu \n Y/N: ")
+    if know_causes == "Y":
+        print(*cold_flu_causes,sep='\n')
+    else:
+        Break()
+
+    know_treament = input("Would you like to know the treatment of cold and flu \n Y/N: ")
+    if know_treament == "Y":
+        print(*cold_flu_treatment,sep='\n')
+    else:
+        Break()
+
+
+elif set(user_symptoms).issubset(set(diarrhea_symptoms)):
+    print("Likely to have diarrhea")
+    know_causes = input("Would you like to know the causes of diarrhea \n Y/N: ")
+    if know_causes == "Y":
+        print(*diarrhea_causes,sep='\n')
+    else:
+        Break()
+
+    know_treament = input("Would you like to know the treatment of diarrhea \n Y/N: ")
+    if know_treament == "Y":
+        print(*cold_flu_treatment,sep='\n')
+    else:
+        Break
+
+
+elif set(user_symptoms).issubset(set(malaria_symptoms)):
+    print("Likely to have malaria")
+    know_causes = input("Would you like to know the causes of malaria \n Y/N: ")
+    if know_causes == "Y":
+        print(*malaria_causes,sep='\n')
+    else:
+        Break()
+    
+
+    know_treament = input("Would you like to know the treatment of malaria \n Y/N: ")
+    if know_treament == "Y":
+        print(*malaria_treatment,sep='\n')
+    else:
+        Break()
+else:
+    print("Consult a Doctor")
+
